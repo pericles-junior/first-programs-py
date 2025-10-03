73b44120037b802b5eaa8965aa6b77c73855e74f
Péricles Júnior <periclesmjr@hotmail.com>
2025-10-03 15:26:37 -0300

    creating the game "Guess the Secret Number

---
+++ secrectNumber.py
@@ -0,0 +1,23 @@
+import os
+
+msgWel = 'Welcome to the Secret Number Game 🤔'
+print('-' * len(msgWel))
+print(msgWel)
+print('-' * len(msgWel))
+secret = 9
+attempts = 0
+
+while True:
+  try:
+    shot = int(input('Type the shoot between 1 a 10: '))
+    attempts += 1
+    if shot == secret and attempts == 1:
+      print(f'Congratulations!! You tried {attempts} once. 👏👏👏')
+      break
+    elif shot == secret and attempts > 1:
+      print(f'Congratulations!! You tried {attempts} times. 👏👏👏')
+      break
+    else:
+      print('Wrong! Try again!')
+  except ValueError:
+    print('Invalid attempt! Type just numbers!')
